1ba6afee43a8c38db4aec89a03e6a8f35fc52f08
JS <350172898@qq.com>
2016-05-27 22:52:19 +0800

    ork

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/bookStore/page/Login_register.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html5>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>用户注册</title>
+<base href = "/bookStore/" targer = "_parent">
+<link type = "css/text" rel = "stylesheet" href = "css/Login_register.css"></link>
+</head>
+<body>
+<header class = "head">iotok书店</header>
+<div class = "register">
+ <header>用户注册</header>
+  <div class = "inner">
+	<form>
+		
+		<p>
+			<lable>用户名：</lable><input></input>
+		</p>
+		<p>
+			<lable>密&nbsp;&nbsp;&nbsp;&nbsp;码：</lable><input></input>
+		</p>
+		<p>
+			<lable>验&nbsp;&nbsp;&nbsp;&nbsp;证：</lable><input></input>
+		</p>
+		<p>
+			<lable>邮&nbsp;&nbsp;&nbsp;&nbsp;箱：</lable><input></input>
+		</p>
+		<div class = "button">
+			<button>注&nbsp;&nbsp;&nbsp;&nbsp;册</button>
+		</div>
+	</form>
+  </div>
+</div>
+<div class = "login">
+ <header>用户登陆</header>
+  <div class = "inner">
+	<form>
+		
+		<p>
+			<lable>用户名：</lable><input></input>
+		</p>
+		<p>
+			<lable>密&nbsp;&nbsp;&nbsp;&nbsp;码：</lable><input></input>
+		</p>
+		<p>
+			<lable>验&nbsp;&nbsp;&nbsp;&nbsp;证：</lable><input></input>
+		</p>
+		<br/>
+		<div class = "button">
+			<button>忘记密码</button>
+			<button>登&nbsp;&nbsp;&nbsp;&nbsp;陆</button>
+			
+			
+		</div>
+		
+	</form>
+  </div>
+</div>
+</body>
+</html>
---
+++ bookStore/src/com/iotek/bookStore/userServlet.java
@@ -0,0 +1,73 @@
+package com.iotek.bookStore;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class userServlet extends HttpServer {
+
+	@Override
+	public void bind(InetSocketAddress arg0, int arg1) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public HttpContext createContext(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpContext createContext(String arg0, HttpHandler arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InetSocketAddress getAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Executor getExecutor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeContext(String arg0) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeContext(HttpContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setExecutor(Executor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
